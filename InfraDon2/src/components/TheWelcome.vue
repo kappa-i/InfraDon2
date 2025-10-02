51a06a165c511968ab5885a405c876448c511e86
kappa-i <gaby.kappai@gmail.com>
2025-10-02 15:36:55 +0200

    increment

---
+++ InfraDon2/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+ <script setup lang="ts">
+import { ref } from 'vue';
+
+// DATA - MODEL
+const counter = ref(20)
+
+// METHODS - CONTROLLER
+const increment = () => {
+  counter.value++
+}
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <button @click="increment">Increment</button>
+  <p style="color: white; font-size: 50px;">{{ counter }}</p>
+</template>
