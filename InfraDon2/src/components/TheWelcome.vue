0d464626956940bd6550a684a2e36b0fd847acd7
kappa-i <gaby.kappai@gmail.com>
2025-10-16 13:10:09 +0200

    .

--- InfraDon2/src/components/TheWelcome.vue
+++ InfraDon2/src/components/TheWelcome.vue
@@ -1,5 +1,8 @@
  <script setup lang="ts">
 import { ref } from 'vue';
+import PouchDB from 'pouchdb'
+import { onMounted } from 'vue';
+
 
 // DATA - MODEL
 const counter = ref(20)
@@ -9,6 +12,28 @@ const increment = () => {
   counter.value++
 }
 
+const storage = ref();
+const postsData = ref([]);
+
+
+onMounted(() => {
+  console.log('=> Composant initialisé');
+  initDatabase()
+});
+
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données');
+  const db = new PouchDB('http://admin:admin@localhost:5984/infra_53_1527')
+  if (db) {
+    console.log("Connected to collection : " + db?.name)
+    storage.value = db
+  } else {
+    console.warn('Something went wrong')
+  }
+}
+
+
+
 </script>
 
 <template>
@@ -16,3 +41,4 @@ const increment = () => {
   <button @click="increment">Increment</button>
   <p style="color: white; font-size: 50px;">{{ counter }}</p>
 </template>
+
